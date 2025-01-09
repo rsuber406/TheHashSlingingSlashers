fdfe708a4ad7a160c8baef151fa880c43669875b
kraken444444 <166565143+kraken444444@users.noreply.github.com>
2025-01-09 17:42:50 -0500

    Added gamemanager

---
+++ ShooterGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public Image playerHPBar;
+    public GameObject damagePanel;
+
+  ///  public playerController playerscript;
+    public GameObject player;
+
+    public bool isPaused;
+
+    int goalCount;
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("player");
+ //       playerscript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        goalCount += amount;
+
+        if (goalCount <= 0)
+        {
+            statePause();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+}
+
