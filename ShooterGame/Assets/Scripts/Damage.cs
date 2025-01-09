eee75adc7ec6c2005528afb13220e3f178ceaeaa
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-01-09 17:05:12 -0500

    Changed naming convention

--- ShooterGame/Assets/Scripts/Damage.cs
+++ ShooterGame/Assets/Scripts/Damage.cs
@@ -1,7 +1,3 @@
 using UnityEngine;
 
-public interface Damage
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void TakeDamage(int amount);
-}
+
