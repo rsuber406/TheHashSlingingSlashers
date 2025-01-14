ed7acf8bfb87dc79e55315042928ea0e6a26a081
Gareth Thomas <11306782+forevergareth@users.noreply.github.com>
2025-01-14 00:02:46 -0500

    Wall run first pass, collision checks and enter state

--- ShooterGame/Assets/Scripts/PlayerController.cs
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -13,9 +13,13 @@ public class PlayerController : MonoBehaviour, IDamage
     [SerializeField] LayerMask ignoreLayer;
     [SerializeField] int health;
     [SerializeField] GameObject bullet;
+    [SerializeField] float wallRunSpeed = 10f;
+    [SerializeField] float wallRunDuration = 2f;
+    [SerializeField] float groundCheckDistance = 1.2f;
 
+    [SerializeField] private CollisionInfo collisionInfo;
 
-
+    private WallRunController wallRunHandler;
     Vector3 playerVel;
     Vector3 moveDir;
     int jumpCount;
@@ -23,34 +27,67 @@ public class PlayerController : MonoBehaviour, IDamage
 
     void Start()
     {
-
+        wallRunHandler = new WallRunController(controller, wallRunSpeed, wallRunDuration);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         Movement();
         Sprint();
         Shoot();
+        CheckWallRun();
+        DrawDebugLines();
 
+        Debug.Log("IsGrounded: " + IsGrounded());
+
+        if (wallRunHandler.IsWallRunning() && !IsAgainstWall())
+        {
+            wallRunHandler.EndWallRun();
+        }
     }
 
     void Movement()
     {
-        if (controller.isGrounded)
+        if (IsGrounded())
         {
             jumpCount = 0;
             playerVel = Vector3.zero;
-
         }
-        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
-        controller.Move(moveDir * movementSpeed * Time.deltaTime);
+
+        // If wall running, handle movement against the wall
+        if (wallRunHandler.IsWallRunning())
+        {
+            Vector3 forwardMovement = transform.forward * Input.GetAxis("Vertical");
+            wallRunHandler.Move(forwardMovement);
+        }
+        else
+        {
+            moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
+            controller.Move(moveDir * movementSpeed * Time.deltaTime);
+        }
+
         Jump();
         controller.Move(playerVel * Time.deltaTime);
-        playerVel.y -= gravity * Time.deltaTime;
-
+        if (!wallRunHandler.IsWallRunning())
+        {
+            playerVel.y -= gravity * Time.deltaTime;
+        }
     }
+
+    private bool IsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     void Sprint()
     {
         if (Input.GetButtonDown("Sprint") && !isSprinting)
@@ -78,7 +115,6 @@ public class PlayerController : MonoBehaviour, IDamage
     {
         health -= amount;
         StartCoroutine(FlashDmgScreen());
-       
     }
 
     void Shoot()
@@ -86,17 +122,95 @@ public class PlayerController : MonoBehaviour, IDamage
         if (Input.GetButtonDown("Shoot"))
         {
             Instantiate(bullet, Camera.main.transform.position, Camera.main.transform.rotation);
-
         }
     }
+
     IEnumerator FlashDmgScreen()
     {
         GameManager.instance.FlashDamageScreenOn();
         yield return new WaitForSeconds(0.1f);
         GameManager.instance.FlashDamageScreenOff();
-        if(health <= 0)
+        if (health <= 0)
         {
             GameManager.instance.Lose();
+        }
+    }
+
+    void CheckWallRun()
+    {
+        if (!IsGrounded() && IsAgainstWall())
+        {
+            Debug.Log("WallRun");
+            Vector3 wallNormal = GetWallNormal();
+            wallRunHandler.StartWallRun(wallNormal);
+        }
+    }
+
+    private bool IsAgainstWall()
+    {
+        collisionInfo.Reset();
+
+        // Raycast to the left
+        RaycastHit leftHit;
+        bool isCollidingOnLeftWall = Physics.Raycast(transform.position, -transform.right, out leftHit, 1f);
+        if (isCollidingOnLeftWall && leftHit.collider.CompareTag("Wall"))
+        {
+            collisionInfo.left = true;
+        }
+
+        // Raycast to the right
+        RaycastHit rightHit;
+        bool isCollidingOnRightWall = Physics.Raycast(transform.position, transform.right, out rightHit, 1f);
+        if (isCollidingOnRightWall && rightHit.collider.CompareTag("Wall"))
+        {
+            collisionInfo.right = true;
+        }
+
+        return collisionInfo.left || collisionInfo.right;
+    }
+
+    private Vector3 GetWallNormal()
+    {
+        // Check for left wall collision
+        RaycastHit leftHit;
+        if (Physics.Raycast(transform.position, -transform.right, out leftHit, 1f))
+            return leftHit.normal;
+
+        // Check for right wall collision
+        RaycastHit rightHit;
+        if (Physics.Raycast(transform.position, transform.right, out rightHit, 1f))
+            return rightHit.normal;
+
+        return Vector3.zero;
+    }
+
+    void DrawDebugLines()
+    {
+        // Raycast to the left
+        RaycastHit leftHit;
+        bool isCollidingOnLeftWall = Physics.Raycast(transform.position, -transform.right, out leftHit, 1f);
+        Debug.DrawRay(transform.position, -transform.right, isCollidingOnLeftWall ? Color.red : Color.green);
+
+        // Raycast to the right
+        RaycastHit rightHit;
+        bool isCollidingOnRightWall = Physics.Raycast(transform.position, transform.right, out rightHit, 1f);
+        Debug.DrawRay(transform.position, transform.right, isCollidingOnRightWall ? Color.red : Color.green);
+
+        // Draw a raycast to the ground
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.blue);
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+        public bool backward, forward;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+            backward = forward = false;
         }
     }
 }
