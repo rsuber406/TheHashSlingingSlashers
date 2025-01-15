b82da62aceb60a6a6ebc463593a590955d6bcd50
luisnxnez <169059992+luisnxnez@users.noreply.github.com>
2025-01-15 18:58:30 -0500

    Slide and crouch functions implemented to playerController

--- ShooterGame/Assets/Scripts/PlayerController.cs
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, IDamage
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] int movementSpeed;
-    [SerializeField] int sprintMod;
+    [SerializeField] CharacterController controller;
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject firearm;
+    [SerializeField] Transform shootPos;
+    [SerializeField] LayerMask ignoreLayer;
+
+    [SerializeField] const int maxHealth = 0;
+    [SerializeField] int health;
+
+    // movement
+    [SerializeField] float movementSpeed;
+    [SerializeField] float sprintMod;
+
+    // jump
     [SerializeField] int jumpSpeed;
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
-    [SerializeField] CharacterController controller;
-    [SerializeField] LayerMask ignoreLayer;
-    [SerializeField] int health;
-    [SerializeField] GameObject bullet;
-    [SerializeField] Transform shootPos;
-    [SerializeField] GameObject firearm;
-    [SerializeField] const int maxHealth = 0;
 
+    // crouch
+    [SerializeField] float crouchHeight;
+    [SerializeField] float crouchMovementSpeed;
+    [SerializeField] float crouchSpeed;
 
+    // slide
+    [SerializeField] float slideMod;
+    [SerializeField] float slideMomentum;   // lower number more further you go
+    [SerializeField] float slideDuration;
+    [SerializeField] float slideThreshold;
 
     private int previousHealth;
+
     Vector3 playerVel;
     Vector3 moveDir;
+
+    float origMovementSpeed;
+    float origHeight;
+    float slideTimer;
     int jumpCount;
-    bool isSprinting;
+    bool isSprinting, isCrouching, isSliding;
+
     GunScripts firearmScript;
     void Start()
     {
         firearmScript = firearm.GetComponent<GunScripts>();
+        origHeight = controller.height;
+        origMovementSpeed = movementSpeed;
     }
 
     // Update is called once per frame
@@ -36,6 +58,8 @@ public class PlayerController : MonoBehaviour, IDamage
         Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 50, Color.red);
         Movement();
         Sprint();
+        Crouch();
+        Slide();
         Shoot();
 
     }
@@ -46,27 +70,48 @@ public class PlayerController : MonoBehaviour, IDamage
         {
             jumpCount = 0;
             playerVel = Vector3.zero;
-
         }
+
+        else
+            // cannot crouch while player in air
+            if (playerVel.y > 0)
+                isCrouching = false;
+
         moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
         controller.Move(moveDir * movementSpeed * Time.deltaTime);
+
         Jump();
+
         controller.Move(playerVel * Time.deltaTime);
         playerVel.y -= gravity * Time.deltaTime;
+        
         shootPos.transform.rotation = Camera.main.transform.rotation;
 
     }
+
     void Sprint()
     {
-        if (Input.GetButtonDown("Sprint") && !isSprinting)
+        if (!isSliding)
         {
-            isSprinting = true;
-            movementSpeed = movementSpeed * sprintMod;
-        }
-        else if (Input.GetButtonUp("Sprint"))
-        {
-            isSprinting = false;
-            movementSpeed = movementSpeed / sprintMod;
+            if (Input.GetButtonDown("Sprint"))
+            {
+                // toggles crouch
+                if (isCrouching)
+                {
+                    isCrouching = false;
+                    Crouch();
+                    movementSpeed = origMovementSpeed;
+                }
+
+                isSprinting = true;
+                movementSpeed *= sprintMod;
+            }
+
+            if (Input.GetButtonUp("Sprint"))
+            {
+                isSprinting = false;
+                movementSpeed = origMovementSpeed;
+            }
         }
     }
 
@@ -99,6 +144,7 @@ public class PlayerController : MonoBehaviour, IDamage
 
         }
     }
+
     IEnumerator FlashDmgScreen()
     {
         if (previousHealth > health)
@@ -117,4 +163,92 @@ public class PlayerController : MonoBehaviour, IDamage
             GameManager.instance.Lose();
         }
     }
+
+    void Crouch()
+    {
+        if (!isSliding)
+        {
+            if (Input.GetButtonDown("Crouch") && !isSprinting)
+                isCrouching = !isCrouching;
+
+            if (isCrouching)
+            {
+                controller.height -= crouchSpeed * Time.deltaTime;
+
+                if (controller.height <= crouchHeight)
+                {
+                    movementSpeed = crouchMovementSpeed;
+                    controller.height = crouchHeight;
+                }
+            }
+
+            else if (!isCrouching && !isSliding)
+            {
+                controller.height += crouchSpeed * Time.deltaTime;
+
+                // was Supposed to stop the jitter from un crouching
+                /* if (controller.height < normalHeight)
+                    player.position += offset * Time.deltaTime;*/
+
+                if (controller.height >= origHeight)
+                {
+                    controller.height = origHeight;
+
+                    if (!isSprinting)
+                        movementSpeed = origMovementSpeed;
+
+                }
+            }
+        }
+    }
+
+
+    void Slide()
+    {
+        if (isSprinting && Input.GetButtonDown("Crouch"))
+        {
+            // Start sliding
+            isSliding = true;
+            isCrouching = false;
+            isSprinting = false;
+
+            slideTimer = 0f;
+            controller.height = crouchHeight;
+
+            movementSpeed = origMovementSpeed;
+            movementSpeed *= slideMod;
+        }
+
+        else if (isSliding)
+        {
+            // slide cancel
+            if (Input.GetButtonDown("Crouch"))
+            {
+                isCrouching = false;
+                isSliding = false;
+                movementSpeed = origMovementSpeed;
+
+            }
+
+            // Continue sliding
+            else
+            {
+                controller.Move(moveDir * slideMod * Time.deltaTime);
+
+                slideTimer += Time.deltaTime;
+                movementSpeed -= slideMod * slideMomentum * Time.deltaTime;
+
+            }
+
+            // End slide if timer exceeds duration or player's speed drops below threshold
+            if ((slideTimer >= slideDuration || movementSpeed < slideThreshold) && isSliding)
+            {
+                movementSpeed = origMovementSpeed;
+                isSliding = false;
+                isCrouching = true;
+
+            }
+        }
+    }
 }
+
