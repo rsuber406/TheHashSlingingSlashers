64e0ceff991c6b16c3523e1def3b5448d95d52e7
kraken444444 <166565143+kraken444444@users.noreply.github.com>
2025-01-26 17:18:08 -0500

    updated bullet time
    updated bullet time to work with the grappling hook and wall running. the players wall run speed and grappling hook speed are effectively doubled during bullet time.

--- ShooterGame/Assets/Scripts/PlayerController.cs
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -71,6 +71,8 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
     private int maxHealth = 100;
     private bool hasTakenDmg;
     int gunListPosition = 0;
+    private float originalGrappleSpeed;
+    private float originalWallRunSpeed;
 
     float origMovementSpeed;
     float origHeight;
@@ -92,11 +94,18 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
     
     void Start()
     {
+
+
+
+
         playerCamera = Camera.main;
         // w/e this shit is
         health = maxHealth;
         origHeight = controller.height;
         origMovementSpeed = movementSpeed;
+        originalGrappleSpeed = forwardGrappleForce;
+        originalWallRunSpeed = wallRunSpeed;
+        
 
         //other shit
         Timesincereload = Time.time + 10000;
@@ -706,6 +715,25 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
         {
             lineRenderer.enabled = false;
         }
+    }
+    public void DoubleGrappleSpeed()
+    {
+        forwardGrappleForce *= 2f;
+        upwardGrappleArkForce *= 2f;
+    }
+
+    public void ResetGrappleSpeed()
+    {
+        forwardGrappleForce /= 2f;
+        upwardGrappleArkForce /= 2f;
+    }
+    public void DoubleWallRunSpeed()
+    {
+        wallRunSpeed *= 2f;
+    }
+    public void ResetWallRunSpeed()
+    {
+        wallRunSpeed /= 2f;
     }
 }
 
